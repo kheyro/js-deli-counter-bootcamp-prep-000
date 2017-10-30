6ab3c4008d9270f94cc0857d5f66a8961edea948
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 17:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    for (let i = 0; i < katzDeliLine.length ; i++) {
+      queue += ` ${i}. ${katzDeliLine[i]},` ;
+    }
+    return `The line is currently:${queue}`;
+  } else {
+    return "The line is currently empty."
+  }
+}
